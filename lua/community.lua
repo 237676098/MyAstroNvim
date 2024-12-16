eac2372c3c3214c377c055ece6dc92b5c7fd77d2
liuxiaoran <liuxiaoran01@zuoyebang.com>
2024-12-16 20:51:03 +0800

    add notice-vim&&cmp-cmdline

--- lua/community.lua
+++ lua/community.lua
@@ -8,9 +8,14 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  --查看某一行的git提交记录 
   { import = "astrocommunity.git.git-blame-nvim" },
+  --快速的给单词加双引号/单引号等 
   { import = "astrocommunity.motion.mini-surround" },
-  -- { import = "astrocommunity.motion.lua" },
+  --定制命令行/通知/弹出菜单的ui
+  { import = "astrocommunity.utility.noice-nvim" },
+  --命令行模式补全提示
+  { import = "astrocommunity.completion.cmp-cmdline" },
   -- { import = "astrocommunity.workflow.bad-practices-nvim" },
   -- import/override with your plugins folder
 }
